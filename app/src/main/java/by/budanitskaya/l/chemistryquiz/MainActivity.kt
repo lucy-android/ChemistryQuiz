b055179e8adff0c26ff7828ca0b8afc32dd79e8d
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-09-10 19:16:05 +0300

    FUN-1: optimized code, extracted Navigator.kt

--- app/src/main/java/by/budanitskaya/l/chemistryquiz/MainActivity.kt
+++ app/src/main/java/by/budanitskaya/l/chemistryquiz/MainActivity.kt
@@ -1,63 +1,18 @@
 package by.budanitskaya.l.chemistryquiz
 
 
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import by.budanitskaya.l.chemistryquiz.databinding.ActivityMainBinding
-import by.budanitskaya.l.chemistryquiz.games.GamesFragment
-import by.budanitskaya.l.chemistryquiz.home.HomeFragment
-import by.budanitskaya.l.chemistryquiz.notifications.NotificationsFragment
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.google.android.material.navigation.NavigationBarView
 
 
-class MainActivity : AppCompatActivity(R.layout.activity_main), NavigationBarView.OnItemSelectedListener {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
 
-    private var homeFragment: HomeFragment? = null
-        get() = if (field == null) HomeFragment() else field
-
-    private var gamesFragment: GamesFragment? = null
-        get() = if (field == null) GamesFragment() else field
-
-    private var notificationFragment: NotificationsFragment? = null
-        get() = if (field == null) NotificationsFragment() else field
-
     override fun onStart() {
         super.onStart()
-        loadFragment(HomeFragment())
-
-        val navigation = binding.navView
-        navigation.setOnItemSelectedListener(this)
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        var fragment: Fragment? = null
-        when (item.itemId) {
-            R.id.navigation_home -> fragment = HomeFragment()
-            R.id.navigation_games-> fragment = GamesFragment()
-            R.id.navigation_notifications -> fragment = NotificationsFragment()
-        }
-        return loadFragment(fragment)
-    }
-
-    private fun loadFragment(fragment: Fragment?): Boolean {
-        if (fragment != null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.nav_host_fragment_activity_main, fragment)
-                .commit()
-            return true
-        }
-        return false
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        homeFragment = null
-        gamesFragment = null
-        notificationFragment = null
+        val navigator = Navigator(supportFragmentManager, binding.navView)
+        this.lifecycle.addObserver(navigator)
     }
 }
---
+++ app/src/main/java/by/budanitskaya/l/chemistryquiz/Navigator.kt
@@ -0,0 +1,64 @@
+package by.budanitskaya.l.chemistryquiz
+
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import by.budanitskaya.l.chemistryquiz.games.GamesFragment
+import by.budanitskaya.l.chemistryquiz.home.HomeFragment
+import by.budanitskaya.l.chemistryquiz.notifications.NotificationsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
+
+class Navigator(
+    private val fragmentManager: FragmentManager,
+    private val navView: BottomNavigationView
+) :
+    DefaultLifecycleObserver, NavigationBarView.OnItemSelectedListener {
+
+    private var homeFragment: HomeFragment? = null
+        get() = if (field == null) HomeFragment() else field
+
+    private var gamesFragment: GamesFragment? = null
+        get() = if (field == null) GamesFragment() else field
+
+    private var notificationFragment: NotificationsFragment? = null
+        get() = if (field == null) NotificationsFragment() else field
+
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        loadFragment(homeFragment)
+        val navigation = navView
+        navigation.setOnItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+        when (item.itemId) {
+            R.id.navigation_home -> fragment = homeFragment
+            R.id.navigation_games -> fragment = gamesFragment
+            R.id.navigation_notifications -> fragment = notificationFragment
+        }
+        return loadFragment(fragment)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            fragmentManager
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment_activity_main, fragment)
+                .commit()
+            return true
+        }
+        return false
+    }
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        homeFragment = null
+        gamesFragment = null
+        notificationFragment = null
+    }
+
+}
