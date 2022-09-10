eda26b07ce23c7e1ed5ff61fc36d2f0f027476f7
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-09-10 18:52:22 +0300

    FUN-1: removed the usages of navigation library and removed the dependency, implemented bottom navigation without bottom navigation

--- app/src/main/java/by/budanitskaya/l/chemistryquiz/MainActivity.kt
+++ app/src/main/java/by/budanitskaya/l/chemistryquiz/MainActivity.kt
@@ -1,36 +1,46 @@
 package by.budanitskaya.l.chemistryquiz
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import by.budanitskaya.l.chemistryquiz.databinding.ActivityMainBinding
-import by.kirich1409.viewbindingdelegate.viewBinding
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import by.budanitskaya.l.chemistryquiz.games.GamesFragment
+import by.budanitskaya.l.chemistryquiz.home.HomeFragment
+import by.budanitskaya.l.chemistryquiz.notifications.NotificationsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
 
-    private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
-
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        loadFragment(HomeFragment())
 
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home,
-                R.id.navigation_games,
-                R.id.navigation_notifications
-            )
-        )
-        setSupportActionBar(binding.toolbar)
+        val navigation = findViewById<BottomNavigationView>(R.id.nav_view)
+        navigation.setOnItemSelectedListener(this)
+    }
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        var fragment: Fragment? = null
+        when (item.itemId) {
+            R.id.navigation_home -> fragment = HomeFragment()
+            R.id.navigation_games-> fragment = GamesFragment()
+            R.id.navigation_notifications -> fragment = NotificationsFragment()
+        }
+        return loadFragment(fragment)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment_activity_main, fragment)
+                .commit()
+            return true
+        }
+        return false
     }
 }
