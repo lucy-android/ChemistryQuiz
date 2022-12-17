05560460266302ef16fe3958789efae7e9576411
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2022-12-17 11:41:37 +0300

    FUN-2: added code to hide data loading with splashscreen, made the static splashscreen work on Android 31+ and below, styled icon for splash, added letters vector

--- app/src/main/java/by/budanitskaya/l/chemistryquiz/MainActivity.kt
+++ app/src/main/java/by/budanitskaya/l/chemistryquiz/MainActivity.kt
@@ -1,21 +1,37 @@
 package by.budanitskaya.l.chemistryquiz
 
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.DecelerateInterpolator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import by.budanitskaya.l.chemistryquiz.databinding.ActivityMainBinding
 import by.kirich1409.viewbindingdelegate.viewBinding
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
 
+    var contentHasLoaded = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
-        installSplashScreen()
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startLoadingContent()
+
+        setupSplashScreen(splashScreen)
     }
 
 
@@ -23,5 +39,41 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onStart()
         val navigator = Navigator(supportFragmentManager, binding.navView)
         this.lifecycle.addObserver(navigator)
+    }
+
+    private fun startLoadingContent() {
+        // For this example, the Timer delay represents awaiting a response from a network call
+        Timer().schedule(3000) {
+            contentHasLoaded = true
+        }
+    }
+
+    private fun setupSplashScreen(splashScreen: SplashScreen) {
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (contentHasLoaded) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else false
+                }
+            }
+        )
+
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideBack = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.TRANSLATION_X,
+                0f,
+                -splashScreenView.view.width.toFloat()
+            ).apply {
+                interpolator = DecelerateInterpolator()
+                duration = 800L
+                doOnEnd { splashScreenView.remove() }
+            }
+
+            slideBack.start()
+        }
     }
 }
