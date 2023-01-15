2b2286d6a41a66793ab372c9c834ed426d1605f3
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2023-01-15 12:54:32 +0300

    FUN-3: added splashscreen to login page, refactored, simplified functionality

--- app/src/main/java/by/budanitskaya/l/chemistryquiz/LoginActivity.kt
+++ app/src/main/java/by/budanitskaya/l/chemistryquiz/LoginActivity.kt
@@ -5,23 +5,52 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.ErrorCodes
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
-    companion object {
-        private const val RC_SIGN_IN = 123
-    }
+
+    private var resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode != Activity.RESULT_OK) {
+                return@registerForActivityResult
+            }
+
+            val response = IdpResponse.fromResultIntent(it.data)
+            if (it.resultCode == Activity.RESULT_OK) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                if (response == null) {
+                    finish()
+                }
+                if (response?.error?.errorCode == ErrorCodes.NO_NETWORK) {
+                    return@registerForActivityResult
+                }
+                if (response?.error?.errorCode == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(this, response.error?.errorCode.toString(), Toast.LENGTH_LONG)
+                        .show()
+                    Log.d(getString(R.string.error_code), response.error?.errorCode.toString())
+                    return@registerForActivityResult
+                }
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         if (FirebaseAuth.getInstance().currentUser == null) {
             createSignInIntent()
         } else {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
             startActivity(intent)
         }
     }
@@ -31,43 +60,21 @@ class LoginActivity : AppCompatActivity() {
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.AnonymousBuilder().build()
         )
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .setIsSmartLockEnabled(false)
-                .setLogo(R.drawable.chemistry_logo)
-                .setTheme(R.style.AuthTheme)
-                .setTosAndPrivacyPolicyUrls(
-                    getString(R.string.tos_url),
-                    getString(R.string.privacy_policy)
-                )
-                .build(),
-            RC_SIGN_IN
-        )
+
+        val intent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .setIsSmartLockEnabled(false)
+            .setLogo(R.drawable.chemistry_logo)
+            .setTheme(R.style.AuthTheme)
+            .setTosAndPrivacyPolicyUrls(
+                getString(R.string.tos_url),
+                getString(R.string.privacy_policy)
+            )
+            .build()
+
+        resultLauncher.launch(intent)
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == RC_SIGN_IN) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-            } else {
-                if (response == null) {
-                    finish()
-                }
-                if (response?.error?.errorCode == ErrorCodes.NO_NETWORK) {
-                    return
-                }
-                if (response?.error?.errorCode == ErrorCodes.UNKNOWN_ERROR) {
-                    Toast.makeText(this, response.error?.errorCode.toString(), Toast.LENGTH_LONG)
-                        .show()
-                    Log.d(getString(R.string.error_code), response.error?.errorCode.toString())
-                    return
-                }
-            }
-        }
-    }
 }
--- app/src/main/java/by/budanitskaya/l/chemistryquiz/MainActivity.kt
+++ app/src/main/java/by/budanitskaya/l/chemistryquiz/MainActivity.kt
@@ -1,49 +1,25 @@
 package by.budanitskaya.l.chemistryquiz
 
 
-import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewTreeObserver
-import android.view.animation.DecelerateInterpolator
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.animation.doOnEnd
-import androidx.core.splashscreen.SplashScreen
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.splashscreen.SplashScreenViewProvider
 import by.budanitskaya.l.chemistryquiz.databinding.ActivityMainBinding
 import by.budanitskaya.l.chemistryquiz.firebase.FirebaseAuthHelperImpl
 import by.budanitskaya.l.chemistryquiz.viewbindingdelegate.viewBinding
 import com.firebase.ui.auth.AuthUI
-import java.util.*
-import java.util.concurrent.atomic.AtomicBoolean
-import kotlin.concurrent.schedule
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    companion object {
-        const val SPLASH_SCREEN_DELAY = 3000L
-        const val ANIMATION_DELAY = 1500
-    }
 
     private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
-
-    private var contentHasLoaded: AtomicBoolean = AtomicBoolean(false)
 
     private val firebaseAuthHelper = FirebaseAuthHelperImpl(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         (application as ChemistryApp).appComponent.inject(this)
-        val splashScreen = installSplashScreen()
-
         super.onCreate(savedInstanceState)
-
-        startLoadingContent()
-
-        setupSplashScreen(splashScreen)
     }
 
     override fun onStart() {
@@ -54,47 +30,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         this.lifecycle.addObserver(navigator)
     }
 
-    private fun startLoadingContent() {
-        Timer().schedule(SPLASH_SCREEN_DELAY) { contentHasLoaded.compareAndSet(false, true) }
-    }
-
-    private fun setupSplashScreen(splashScreen: SplashScreen) {
-        val content = binding.root
-        content.viewTreeObserver.addOnPreDrawListener(
-            object : ViewTreeObserver.OnPreDrawListener {
-                override fun onPreDraw(): Boolean {
-                    return if (contentHasLoaded.get()) {
-                        content.viewTreeObserver.removeOnPreDrawListener(this)
-                        true
-                    } else false
-                }
-            }
-        )
-
-        splashScreen.setOnExitAnimationListener { splashScreenView ->
-            val slideBack = ObjectAnimator.ofFloat(
-                splashScreenView.view,
-                View.TRANSLATION_Y,
-                0f,
-                -splashScreenView.view.height.toFloat()
-            ).apply {
-                interpolator = DecelerateInterpolator()
-                duration = calculateRemainingAnimationDuration(splashScreenView) + ANIMATION_DELAY
-                doOnEnd { splashScreenView.remove() }
-            }
-
-            slideBack.start()
-        }
-    }
-
-    private fun calculateRemainingAnimationDuration(splashScreenView: SplashScreenViewProvider): Long {
-        val firstAnimationExpectedDuration = splashScreenView.iconAnimationDurationMillis
-        val firstAnimationStart = splashScreenView.iconAnimationStartMillis
-        val firstAnimationEnd = System.currentTimeMillis()
-        return (firstAnimationExpectedDuration + firstAnimationStart - firstAnimationEnd)
-            .coerceAtLeast(0L)
-    }
-
 
     fun signOut() {
         AuthUI.getInstance()
@@ -103,7 +38,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 val intent = Intent(this, LoginActivity::class.java)
                 this.startActivity(intent)
                 this.finish()
-                Toast.makeText(this, "Successfully Log Out", Toast.LENGTH_SHORT).show()
             }
     }
 
