98cdd4a0ebfd4d7a906ee51e2e2f29dcacf55e70
liudmila-budanitskaya-code <Liudmila28march1991@gmail.com>
2023-02-26 17:04:53 +0300

    FUN-4: painted a circle sector

--- app/src/main/java/by/budanitskaya/l/chemistryquiz/ui/view/custom/CountdownClockView.kt
+++ app/src/main/java/by/budanitskaya/l/chemistryquiz/ui/view/custom/CountdownClockView.kt
@@ -1,20 +1,26 @@
 package by.budanitskaya.l.chemistryquiz.ui.view.custom
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import kotlin.math.min
 
+
 class CountdownClockView(context: Context, attributeSet: AttributeSet) :
     View(context, attributeSet) {
 
-    private fun calculatePaint(width: Float) = Paint().apply {
+    private fun calculateStrokePaint(width: Float) = Paint().apply {
         color = Color.BLACK
         style = Paint.Style.STROKE
         strokeWidth = width
+    }
+
+    private fun calculateArcPaint() = Paint().apply {
+        color = Color.BLUE
+        strokeWidth = 5f
+        style = Paint.Style.FILL
+        isAntiAlias = true
     }
 
     override fun onDraw(canvas: Canvas?) {
@@ -22,12 +28,22 @@ class CountdownClockView(context: Context, attributeSet: AttributeSet) :
 
         val centreX = (right - left).toDouble() / 2
         val centreY = (bottom - top).toDouble() / 2
-        val strokeWidth = min(centreX, centreY).toFloat() / 10
+        val rimWidth = min(centreX, centreY).toFloat() / 10
+        val radius = (min(right - left, bottom - top).toFloat()) / 2
+        val radiusSmall = radius - rimWidth
+
         canvas?.drawCircle(
             centreX.toFloat(),
             centreY.toFloat(),
-            (min(right - left, bottom - top).toFloat()) / 2 - strokeWidth,
-            calculatePaint(strokeWidth)
+            radius - rimWidth / 2,
+            calculateStrokePaint(rimWidth)
         )
+
+
+        val oval = RectF()
+        oval[(centreX - radiusSmall).toFloat(), (centreY - radiusSmall).toFloat(), (centreX + radiusSmall).toFloat()] =
+            (centreY + radiusSmall).toFloat()
+        canvas?.drawArc(oval, 0F, 270F, true, calculateArcPaint())
+
     }
 }
